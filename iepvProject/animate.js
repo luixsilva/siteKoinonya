5efadcdc96e9dc71a235ecaeee22dd7d67a0b534
luixsilva <113793929+luixsilva@users.noreply.github.com>
2024-06-04 17:58:05 -0300

    Add files via upload

---
+++ iepvProject/animate.js
@@ -0,0 +1,46 @@
+var words = [
+    "Mas ele foi ferido por causa das nossas transgressões e moído por causa das nossas iniquidades o castigo que nos traz a paz estava sobre ele e pelas suas pisaduras fomos sarados",
+  ],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
